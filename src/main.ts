2c88fb4d009e97f6466e02d51f7a842b7ffc07c3
lazicmarko <markolazic1995@gmail.com>
2023-03-11 10:16:55 +0100

    initial config

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+import * as morgan from 'morgan'
+import { ValidationPipe } from '@nestjs/common/pipes'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule)
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  )
+  app.use(morgan('dev'))
+  await app.listen(3000)
 }
-bootstrap();
+bootstrap()
