4ce69f46922ad4e16d83097ab9670dd6fe62dc28
lazicmarko <markolazic1995@gmail.com>
2023-12-14 19:56:12 +0100

    added db connection & swagger init

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,42 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import * as Joi from '@hapi/joi'
+
+import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { MongooseModule } from '@nestjs/mongoose'
+
+import { FixtureModule } from './fixture/fixture.module'
+import { PlayerModule } from './player/player.module'
+import { SquadModule } from './squad/squad.module'
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        APP_ENV: Joi.string().required(),
+        MONGO_USERNAME: Joi.string().required(),
+        MONGO_PASSWORD: Joi.string().required(),
+        MONGO_DATABASE: Joi.string().required(),
+        MONGO_HOST: Joi.string().required(),
+      }),
+    }),
+    MongooseModule.forRootAsync({
+      useFactory: (configService: ConfigService) => {
+        const username = configService.get('MONGO_USERNAME')
+        const password = configService.get('MONGO_PASSWORD')
+        const database = configService.get('MONGO_DATABASE')
+        const host = configService.get('MONGO_HOST')
+
+        return {
+          uri: `mongodb+srv://${username}:${password}@${host}`,
+          dbName: database,
+        }
+      },
+      inject: [ConfigService],
+    }),
+    PlayerModule,
+    FixtureModule,
+    SquadModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
-import { NestFactory } from '@nestjs/core'
-import { AppModule } from './app.module'
 import * as morgan from 'morgan'
+import 'reflect-metadata'
+
 import { ValidationPipe } from '@nestjs/common/pipes'
+import { NestFactory } from '@nestjs/core'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+
+import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const config = new DocumentBuilder()
+    .setTitle('Termin')
+    .setDescription('The Termin API description')
+    .setVersion('1.0')
+    .addTag('termin')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
 
+  SwaggerModule.setup('docs', app, document)
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -18,5 +30,6 @@ async function bootstrap() {
   )
   app.use(morgan('dev'))
   await app.listen(3000)
+  console.log(`Application is running on: ${await app.getUrl()}`)
 }
 bootstrap()
